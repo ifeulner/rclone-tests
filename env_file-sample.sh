0aa0b3a1b3e4c3c36e1465bb696a564d035cf794
Ingo Feulner <ingo.feulner@eng-softwarelabs.de>
2020-01-27 09:25:01 +0100

    added sample

---
+++ env_file-sample.sh
@@ -0,0 +1,5 @@
+export RCLONE_CONFIG_MEDTST_TYPE=webdav
+export RCLONE_CONFIG_MEDTST_URL=https://nextcloud.domain.com/remote.php/webdav/
+export RCLONE_CONFIG_MEDTST_VENDOR=nextcloud
+export RCLONE_CONFIG_MEDTST_USER=<username>
+export RCLONE_CONFIG_MEDTST_PASS=<redacted>
